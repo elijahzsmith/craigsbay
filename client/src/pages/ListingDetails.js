8e8b8f4eb4158e2a8043a15966f59268ae9f6621
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2022-05-27 17:28:16 -0400

    Made details page more responsive

--- client/src/pages/ListingDetails.js
+++ client/src/pages/ListingDetails.js
@@ -12,7 +12,7 @@ function ListingDetails() {
     <Container>
 
       <Row className='mt-3 mb-1'>
-        <Col className='col-8'>
+        <Col className='col-md-8 text-md-start col-12 text-center'>
           <h1>{what_it_is}</h1>
         </Col>
         <Col className='text-center d-flex justify-content-center align-items-center text-secondary'>
