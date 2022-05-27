f83203af3b34531205828d0db023cd47c2446003
CharlieRojas <crc_185@yahoo.com>
2022-05-27 14:38:37 -0500

    working toggle for alphabetized filter

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,6 +7,7 @@ import ListingItem from "../components/ListingItem";
 function Home({ user, handleCardClick }) {
   const [listings, setListings] = useState([])
   const [homeLoaded, setIsHomeLoaded] = useState(false)
+  const [filtered, setFiltered] = useState(false)
 
   useEffect(() => {
     fetch("/listings")
@@ -19,8 +20,27 @@ function Home({ user, handleCardClick }) {
 
   console.log(listings)
 
+  const handleSortAlphabetically = () => {
+    if (filtered === false) {
+      const sortedListings = listings.sort(function(a, b){
+        let x = a.what_it_is.toLowerCase();
+        let y = b.what_it_is.toLowerCase();
+        if (x < y) {return -1;}
+        if (x > y) {return 1;}
+        return 0;
+      })
+      setListings(sortedListings)
+      setFiltered(true)
+    } else {
+      const originalArray = listings.sort(function(a, b){return a.id - b.id})
+      setListings(originalArray)
+      setFiltered(false)
+    }
+  }
+  
   if (!homeLoaded) return <h3>Loading...</h3>
 
+  
   const renderListings = listings.map((listing) => {
     return (
       <ListingItem key={listing.id} listing={listing} user={user} handleCardClick={handleCardClick} />
@@ -29,6 +49,7 @@ function Home({ user, handleCardClick }) {
 
   return (
     <Container fluid>
+      <button onClick={ () => handleSortAlphabetically()}>hey</button>
       <Row xs={1} sm={2} md={3} lg={4}>
         {renderListings}
       </Row>
