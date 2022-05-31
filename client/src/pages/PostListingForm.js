b30627b9572984952e8c731c5f55ecf2b339bf56
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2022-05-31 18:36:53 -0400

    Added default values forcing a selection on Post Listing form

--- client/src/pages/PostListingForm.js
+++ client/src/pages/PostListingForm.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
 import Button from "react-bootstrap/esm/Button";
 import Form from "react-bootstrap/esm/Form";
 
@@ -14,15 +15,21 @@ function PostListingForm({ user }) {
     what_it_is: "",
     category: "",
     description: "",
-    end_time: "",
+    month: "",
+    day: "",
+    year: "",
+    time: "",
     user_id: user.id,
   });
 
   const [timerID, setTimerID] = useState(null)
 
   const handleChange = (e) => {
+    console.log(e.target)
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
+
+  // console.log(formData)
 
   const configObjPOST = {
     method: "POST",
@@ -30,7 +37,15 @@ function PostListingForm({ user }) {
       "Content-Type": "application/json",
       Accept: "application/json",
     },
-    body: JSON.stringify(formData),
+    body: JSON.stringify({
+      location: formData.location,
+      image_url: formData.image_url,
+      what_it_is: formData.what_it_is,
+      category: formData.category,
+      description: formData.description,
+      end_time: `${formData.month}/${formData.day}/${formData.year} ${formData.time}`,
+      user_id: user.id,
+    }),
   };
 
   const handleAddListing = (e) => {
@@ -38,7 +53,7 @@ function PostListingForm({ user }) {
     fetch("/listings", configObjPOST)
       .then((res) => res.json())
       .then((data) => {
-        // console.log(data);
+        console.log(data);
         setTimerID(data.id)
         setFormData({
           location: "",
@@ -73,6 +88,107 @@ function PostListingForm({ user }) {
       .then((res) => res.json())
       .then((data) => console.log(data))
   }, [timerID])
+
+  function renderMonths() {
+
+    const currDate = new Date()
+    const currMonth = currDate.getMonth()
+
+    let upcomingMonths = []
+
+    for (let i = currMonth + 1; i < 13; i++) {
+      upcomingMonths.push(i)
+    }
+
+    for (let i = 1; i < currMonth + 1; i++) {
+      upcomingMonths.push(i)
+    }
+
+    upcomingMonths.unshift('Select Month')
+
+    const monthOptions = upcomingMonths.map(month => {
+      return <option>{month}</option>
+    })
+
+    console.log(monthOptions[0])
+
+    return monthOptions
+  }
+
+  function renderDays() {
+    const currDate = new Date()
+    const currDay = currDate.getDate()
+
+    let upcomingDays = []
+
+    for (let i = currDay; i < 32; i++) {
+      upcomingDays.push(i)
+    }
+
+    for (let i = 1; i < currDay; i++) {
+      upcomingDays.push(i)
+    }
+
+    upcomingDays.unshift('Select Day')
+
+    const dayOptions = upcomingDays.map(day => {
+      return <option>{day}</option>
+    })
+
+    return dayOptions
+  }
+
+  function renderYears() {
+    const currDate = new Date()
+    const currYear = currDate.getFullYear()
+
+    let upcomingYears = []
+
+    for (let i = currYear; i < (currYear + 5); i++) {
+      upcomingYears.push(i)
+    }
+
+    upcomingYears.unshift('Select Year')
+
+    const yearOptions = upcomingYears.map(year => {
+      return <option>{year}</option>
+    })
+
+    return yearOptions
+  }
+
+  function renderTimes() {
+    const currDate = new Date()
+    const currHour = currDate.getHours()
+
+    let upcomingTimes = []
+
+    for (let i = currHour; i < 24; i++) {
+      for (let j = 0; j < 60; j++)
+        if (j < 10) {
+          upcomingTimes.push(`${i}:0${j}`)
+        } else {
+          upcomingTimes.push(`${i}:${j}`)
+        }
+    }
+
+    for (let i = 0; i < currHour; i++) {
+      for (let j = 0; j < 60; j++)
+        if (j < 10) {
+          upcomingTimes.push(`${i}:0${j}`)
+        } else {
+          upcomingTimes.push(`${i}:${j}`)
+        }
+    }
+
+    upcomingTimes.unshift('Select Time')
+
+    const timeOptions = upcomingTimes.map(time => {
+      return <option>{time}</option>
+    })
+
+    return timeOptions
+  }
 
   return (
     <Container fluid>
@@ -138,16 +254,61 @@ function PostListingForm({ user }) {
               />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>End Date</Form.Label>
-              <Form.Control
-                type="text"
-                name="end_time"
-                placeholder="End date..."
-                value={formData.end_time}
-                onChange={(e) => handleChange(e)}
-              />
-            </Form.Group>
+            <Row>
+
+              <Col>
+                <Form.Group className="mb-3">
+                  <Form.Label>Month</Form.Label>
+                  <Form.Select
+                    name="month"
+                    value={formData.month}
+                    onChange={(e) => handleChange(e)}
+                  >
+                    {renderMonths()}
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+
+              <Col>
+                <Form.Group className="mb-3">
+                  <Form.Label>Day</Form.Label>
+                  <Form.Select
+                    name="day"
+                    value={formData.day}
+                    onChange={(e) => handleChange(e)}
+                  >
+                    {renderDays()}
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+
+              <Col>
+                <Form.Group className="mb-3">
+                  <Form.Label>Year</Form.Label>
+                  <Form.Select
+                    name="year"
+                    value={formData.year}
+                    onChange={(e) => handleChange(e)}
+                  >
+                    {renderYears()}
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+
+              <Col>
+                <Form.Group className="mb-3">
+                  <Form.Label>Time</Form.Label>
+                  <Form.Select
+                    name="time"
+                    value={formData.time}
+                    onChange={(e) => handleChange(e)}
+                  >
+                    {renderTimes()}
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+
+            </Row>
 
             <Row className="d-flex justify-content-center mb-2">
               <Button variant="primary" type="submit" className="w-25">
