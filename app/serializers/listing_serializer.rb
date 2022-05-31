191b967febd16da3816ad6c5450d9d48f6b1a152
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2022-05-31 15:20:51 -0400

    Fixed serializer lol

--- app/serializers/listing_serializer.rb
+++ app/serializers/listing_serializer.rb
@@ -2,7 +2,7 @@ class ListingSerializer < ActiveModel::Serializer
   attributes :id, :location, :image_url, :what_it_is, :category, :description, :user_id, :winner, :end_time
 
   def winner
-    winner = User.find(winner_id)
+    winner = User.find(:winner_id)
   end
-  
+
 end
